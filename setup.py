d323ec21d5a600c6472cb9c33b1747c96d758fbf
NicolaiMogensen <Nicolaimogensen@outlook.com>
2017-07-11 09:36:57 +0200

    Test author

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
+# Test commit message
 # Get the long description from the relevant file
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
